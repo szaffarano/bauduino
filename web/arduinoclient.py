2c212b6293906d50f2e3266d5e431099cb73ea0b
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2014-01-09 19:10:14 -0300

    agrego ejecución para forzar error en slave y testear esa funcionalidad en el rework

--- web/arduinoclient.py
+++ web/arduinoclient.py
@@ -28,17 +28,14 @@ logging.info("Modbus client initialized!")
 
 logging.info("Getting holding registers...")
 ex = client.read_holding_registers(address=0, count=9, unit=client_id);
+
 logging.info("Holding registers values: %s" % ex.registers)
 ex = client.write_registers(1, [1], unit=client_id)
 logging.info("Write registers response: %s" % ex)
-#if ex.registers[0] == 1:
-#    logging.info("Switching off led #13")
-#    value = 0
-#else:
-#    logging.info("Switching on led #13")
-#    value = 1
 
-#ex = client.write_registers(0, [value], unit=client_id)
+logging.info("Getting holding registers with error")
+ex = client.read_holding_registers(address=0, count=20, unit=client_id);
+logging.info("Write registers response with error: %s" % ex)
 
 logging.info("Closing modbus client")
 client.close()
